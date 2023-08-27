9cf420a1082f98c1a11b999f25992907812444d9
dhruvmaithil <dhruvmaithil@gmail.com>
2023-08-28 00:33:53 +0530

    added authentication & adding items by user

--- furnify/settings.py
+++ furnify/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-v$vjlfoiic=d*ck)$%+pskl1n*4ok^u*d4z^&g=bxy-42@l1ig
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Application definition
 
